e996a3a34766fb002d16923944991b390c305654
andyf <andy.frith@rightmove.co.uk>
2019-12-17 11:31:58 +0000

    while loop for running
    change right sensor port

--- robo.py
+++ robo.py
@@ -4,7 +4,7 @@ from gpiozero import LineSensor
 import signal
 
 left_sensor = LineSensor(22)
-right_sensor= LineSensor(24)
+right_sensor= LineSensor(16)
 
 left_motor = eh.motor.one
 right_motor = eh.motor.two
@@ -17,8 +17,6 @@ dry_run = False
 low_speed = 0
 turning_speed = 15
 high_speed = 15
-
-#andy was here
 
 def go_left():
     global dry_run, low_speed, turning_speed, high_speed
@@ -67,13 +65,13 @@ def left_update(state):
     global left_on_line
     print('left update: ' + str(state))
     left_on_line = state
-    update_states()
+    #update_states()
         
 def right_update(state):
     global right_on_line
     print('right update: ' + str(state))
     right_on_line = state
-    update_states()
+    #update_states()
 
 left_sensor.when_line = lambda: left_update(True)
 left_sensor.when_no_line = lambda: left_update(False)
@@ -83,4 +81,6 @@ right_sensor.when_no_line = lambda: right_update(False)
 
 signal.signal(signal.SIGINT, lambda x: stop())
 
-update_states()
+while 1:
+    update_states()
+    time.sleep(0.1)
