19e13efb0e8b81c9760ec201cc058708d6d3951d
andyf <andy.frith@rightmove.co.uk>
2019-12-17 14:40:15 +0000

    change turning

--- robo.py
+++ robo.py
@@ -30,8 +30,8 @@ def go_right():
     global dry_run, low_speed, turning_speed, high_speed, left_motor, right_motor
     print('going right')
     if not dry_run:
-        left_motor.backwards(turning_speed)
-     #   right_motor.forwards(turning_speed)
+     #   left_motor.backwards(turning_speed)
+        right_motor.forwards(turning_speed)
     
 def go_onwards():
     global dry_run, high_speed, left_motor, right_motor
