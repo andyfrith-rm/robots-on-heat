109b491f8974b4c1fa3c2f025a4ebe09a2f20a5f
Robo Pi <robo-pi@rm.co.uk>
2019-12-17 12:04:20 +0000

    working version 0.1

--- robo.py
+++ robo.py
@@ -3,44 +3,44 @@ import time
 from gpiozero import LineSensor
 import signal
 
-left_sensor = LineSensor(22)
-right_sensor= LineSensor(16)
+left_sensor = LineSensor(24)
+right_sensor= LineSensor(25)
 
 left_motor = eh.motor.one
 right_motor = eh.motor.two
 
-left_on_line = True
-right_on_line = True
+left_on_line = False
+right_on_line = False
 
-dry_run = True
+dry_run = False
 
 low_speed = 0
 turning_speed = 15
 high_speed = 15
 
 def go_left():
-    global dry_run, low_speed, turning_speed, high_speed
+    global dry_run, low_speed, turning_speed, high_speed, left_motor, right_motor
     print('going left')
     if not dry_run:
         left_motor.forwards(turning_speed)
         right_motor.forwards(low_speed)
     
 def go_right():
-    global dry_run, low_speed, turning_speed, high_speed
+    global dry_run, low_speed, turning_speed, high_speed, left_motor, right_motor
     print('going right')
     if not dry_run:
         left_motor.forwards(low_speed)
         right_motor.forwards(turning_speed)
     
 def go_onwards():
-    global dry_run, high_speed
+    global dry_run, high_speed, left_motor, right_motor
     print('going onwards')
     if not dry_run:
         left_motor.forwards(high_speed)
         right_motor.forwards(high_speed)
 
 def stop():
-    global dry_run
+    global dry_run, left_motor, right_motor
     print('stop')
     if not dry_run:
         left_motor.stop()
@@ -50,11 +50,13 @@ def update_states():
     global left_on_line
     global right_on_line
 #    print('update_states()')
+    print('left on line: ' + str(left_on_line))
+    print('right on line: ' + str(right_on_line))
     if left_on_line and right_on_line:
 #        print('left on line' + str(left_on_line))
 #        print('right on line' + str(right_on_line))
-        print 'Should not get here, both sensors on line!'
-        stop()
+        print ('Should not get here, both sensors on line!')
+       # stop()
     elif left_on_line:
         go_left()
     elif right_on_line:
@@ -64,15 +66,15 @@ def update_states():
 
 def left_update(state):
     global left_on_line
-#    print('left update: ' + str(state))
+    print('left update: ' + str(state))
     left_on_line = state
-    #update_states()
+    update_states()
         
 def right_update(state):
     global right_on_line
-#    print('right update: ' + str(state))
+    print('right update: ' + str(state))
     right_on_line = state
-    #update_states()
+    update_states()
 
 left_sensor.when_line = lambda: left_update(True)
 left_sensor.when_no_line = lambda: left_update(False)
@@ -80,8 +82,8 @@ left_sensor.when_no_line = lambda: left_update(False)
 right_sensor.when_line = lambda: right_update(True)
 right_sensor.when_no_line = lambda: right_update(False)
 
-signal.signal(signal.SIGINT, lambda x: stop())
+#signal.signal(signal.SIGINT, lambda x: stop())
 
-while 1:
-    update_states()
-    time.sleep(0.1)
+#while 1:
+update_states()
+  #  time.sleep(0.1)
